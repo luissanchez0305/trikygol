970fa9265f89c6bc6015420f47a7f0de663d5f22
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2018-04-14 18:57:48 +0000

    cambio de servidor para el api

--- www/build/main.js
+++ www/build/main.js
@@ -63,6 +63,7 @@ module.exports = webpackAsyncContext;
 //export var Constants = {  };
 var Constants = {
     //API_ENDPOINT : '/ws/',
+    //API_ENDPOINT : 'https://www.esferasoluciones.com/api/trikygol/',
     API_ENDPOINT: 'http://34.212.30.101/api/trikygol/',
     months: [
         'Enero',
